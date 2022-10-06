8ce6fa09fdc1f0b04aa2f4cc7f7409de5664107b
TC5027 <thibert501@gmail.com>
2022-10-06 17:30:48 +0200

    simple setup

--- README.md
+++ README.md
@@ -4,34 +4,6 @@ A step further Liz Rice's lb-from-scratch
 
 ## How to launch ?
 
-In a first terminal:
-git clone this
-then go in src/libbpf
-git submodule init
-git submodule update
-then go in config and sudo docker-compose up
+Clone this repository and then go in src/libbpf then git submodule init and git submodule update then go in config and ./launch.sh
+Now you can run sudo docker exec -it CLIENT bash and inside curl IP_OF_LOAD_BALANCER
 
-In a second terminal:
-go in config 
-chmod+x launch.sh and rename.py if needed
-./launch.sh
-sudo docker exec -it CONTAINER_ID_OF_LB bash
-cd load-balancer
-make
-
-(you can get container id doing sudo docker ps)
-
-In a third terminal:
-sudo docker exec -it CONTAINER_ID_OF_CLIENT bash
-curl IP_ADDRESS_OF_LB
-
-(you can get container ip address doing sudo docker inspect CONTAINER_ID | grep IPAddress)
-
-
-cd src/libbpf et git submodule init et git submodule update
-cd config et ./launch.sh
-ouvrir un terminal et faire l.11 de launch.sh
-ouvrir un terminal et faire cd config et l.12 de launch.sh
-dans le premier terminal faire les lignes 15 et 16 de launch.sh
-dans le premier terminal chercher l'ip du lb avec sudo docker ps et sudo docker inspect ID | grep IPAddress
-dans le deuxieme terminal faire curl cet ip
--- config/launch.sh
+++ config/launch.sh
@@ -6,14 +6,13 @@ touch ../src/xdp_lb_kern.c.tmp
 sudo docker build -t client ./client
 sudo docker build -t lb ./load-balancer
 
-sudo docker run -d --name BACKEND_A nginxdemos/hello:plain-text
-sudo docker run -d --name BACKEND_B nginxdemos/hello:plain-text
-#sudo gnome-terminal -- sudo docker run -it --name CLIENT client
-#sudo gnome-terminal -- sudo docker run -v $PWD/../src:/load-balancer --privileged -it --name LB lb
-# je voudrais pouvoir renommer les terminaux et mettre un flag qui hold https://askubuntu.com/questions/46627/how-can-i-make-a-script-that-opens-terminal-windows-and-executes-commands-in-the 
-# il faudrait que je trouve un moyen de pouvoir attendre la complétion des commandes dans les nouveaux terminaux avant de lancer ./rename.py
-#./rename.py
-#mv ../src/xdp_lb_kern.c.tmp ../src/xdp_lb_kern.c
-# afficher l'ip du loadbalancer
+sudo docker run -d --name BACKEND_A -h BACKEND_A nginxdemos/hello:plain-text
+sudo docker run -d --name BACKEND_B -h BACKEND_B nginxdemos/hello:plain-text
+sudo docker run -dit --name CLIENT client
+sudo docker run -dit --privileged -v $PWD/../src:/load-balancer --name LB lb
+./rename.py
+mv ../src/xdp_lb_kern.c.tmp ../src/xdp_lb_kern.c
+sudo docker exec LB bash -c 'cd load-balancer ; make'
+ip_lb=$(sudo docker inspect LB | grep IPAddress)
+echo "ip address of the load-balancer is: $ip_lb"
 
-#in my profile preferences, in command sub section, when command exits : hold the terminal open
